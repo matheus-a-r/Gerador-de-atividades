f079e68035647e451de65a03233990920af920fd
Matheus <matalves12345@gmail.com>
2025-02-15 12:45:34 -0300

    Utilizando front antigo

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,13 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  module.exports = {
-    webpack: (config) => {
-    config.resolve.alias.canvas = false;
-    
-    return config;
-    },
-  }
 };
 
 export default nextConfig;
